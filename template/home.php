24d60bc76ddb0df90b83e2cba75cda10a758d91f
Roberto Cordeiro Filho <cordeirofilho@gmail.com>
2018-09-12 16:21:16 -0300

    Verifica os filtros

--- template/home.php
+++ template/home.php
@@ -106,7 +106,7 @@ $fulltext_lang['en'] = __('English','leisref');
                 </div>
             </section>
 
-<?php if ($leisref_config[page_layout] != 'whole_page' || $_GET['q'] != '' ) :  // test for page layout and query search?>
+<?php if ($leisref_config[page_layout] != 'whole_page' || $_GET['q'] != '' || $_GET['filter'] != '' ) :  // test for page layout,  query search and Filters ?>
 
             <div class="content-area result-list">
     			<section id="conteudo">
