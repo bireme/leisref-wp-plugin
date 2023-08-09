fe3694d05ecf772128fd418659b2465ba0962064
wilsonmoura <wilsonsmoura@gmail.com>
2023-08-09 15:51:35 -0300

    Fixed #63 -- Compatibilidade com PHP 8

--- lib/Paginator.php
+++ lib/Paginator.php
@@ -7,28 +7,24 @@ class Paginator{
     var $pag = array();
     var $current_page;
 
-    function Paginator($total, $start, $count=10)
+    function __construct($total, $start, $count=10)
     {
         $this->total = $total;
         $this->start = $start;
         $this->count = $count;
-        $this->current_page = ( !empty($_GET['page']) ? $_GET['page'] : 1);
+        $this->current_page = ( !empty($_GET['page']) ? $_GET['page'] : 1 );
     }
 
     function paginate($page_url_params){
         $pag['total'] = $this->total;
         $pag['total_formatted'] = number_format($pag['total'], 0, ',', '.');
         $pag['start'] = $this->start;
-        $pag['total_pages'] = (($pag['total']/$this->count) % 10 == 0) ? (int)($pag['total']/$this->count) : (int)($pag['total']/$this->count+1);
+        $pag['total_pages'] = (($pag['total']/$this->count) % $this->count == 0) ? (int)($pag['total']/$this->count) : (int)($pag['total']/$this->count+1);
         $pag['count'] = $this->count;
-
-
-
 
         $range_min = (($this->current_page-5) > 0) ? $this->current_page-5 : 1;
         $range_max = (($range_min+7) > $pag['total_pages']) ? $pag['total_pages'] : $range_min+7;
         $pag['pages'] = range($range_min, $range_max);
-
 
         $this->output  = '<div class="pagination pagination-centered">';
         $this->output .= '<ul>';
@@ -76,9 +72,7 @@ class Paginator{
 
         $this->output .= '</ul>';
         $this->output .= '</div>';
-
     }
-
 
     function display_pages(){
         return $this->output;
