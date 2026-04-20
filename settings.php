067912603595789be773d9b31b55484d96ca2d0d
wilsonmoura <wilsonsmoura@gmail.com>
2026-04-20 13:55:32 -0300

    #71 -- Opção para ativar/desativar os documentos similares

--- settings.php
+++ settings.php
@@ -174,12 +174,12 @@ function leisref_page_admin() {
 
             $j(function(){
                 $j('#similar-docs').on('change', function() {
-                    $j('#default-filter-db').prop('disabled', this.checked);
-                }).trigger('change');
+                    $j('#default-filter-db').prop('disabled', !this.checked);
+                });
 
                 $j('#related-docs').on('change', function() {
-                    $j('#extra-filter-db').prop('disabled', this.checked);
-                }).trigger('change');
+                    $j('#extra-filter-db').prop('disabled', !this.checked);
+                });
 
                 $j( "ul.droptrue" ).sortable({
                     connectWith: "ul"
